a0c6cdeef21a9bb82002d8ed98b9d49b3f84f3d9
Asami Kubota <asami@concrete5.co.jp>
2021-04-02 12:33:52 +0900

    css追加、ヘッダーフッターincludeファイル追加、ページ一覧作成

---
+++ gs_fctokyo/include/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html class="h-100" lang="ja">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title><?php echo $title ?></title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body class="d-flex flex-column h-100">
+
+    <header>
+        <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+            <div class="container">
+                <a class="navbar-brand" href="/">FC共闘</a>
+                <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#headerNav" aria-controls="headerNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            
+                <div class="navbar-collapse collapse" id="headerNav">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="mypage.php">マイページ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="y_event_list.php">イベント一覧</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">このページについて</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="logout.php">ログアウト</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- /.navbar-collapse --> 
+    </header>
