31f98bf0c05d5860fa03a962bef087202f91a70e
Asami Kubota <asami@concrete5.co.jp>
2021-04-07 10:46:56 +0900

    階層整理

--- gs_fctokyo/y_footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-<p>created by pojico18 of G'sACADEMIY UNIT_SAPPORO_DEV1</p>
-<a href="./y_event_list.php" target="_blank" rel="noopener noreferrer"><button>イベント一覧</button></a>
-<br>
-<a href="./mypage.php" target="_blank" rel="noopener noreferrer"><button>マイページ</button></a>
-<br>
-<a href="./logout.php">ログアウト</a>
-</footer>
